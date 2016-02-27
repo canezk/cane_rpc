9a4774d9bb20489b400205c668a40e7e4a529d20
zhoukang <zhoukang199191@126.com>
2016-02-27 16:37:24 +0800

    ADD:base interface for server implementation

---
+++ src/com/cane/rpc/server/Server.java
@@ -0,0 +1,16 @@
+/**
+ * @(#)Server.java, 2016年2月27日. 
+ * 
+ * Copyright 2016 Yodao, Inc. All rights reserved.
+ * YODAO PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com.cane.rpc.server;
+
+/**
+ * Base interface for all server implementation
+ * @author zhoukang
+ *
+ */
+public interface Server {
+
+}
