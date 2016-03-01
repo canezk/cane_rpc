0d9f643b630f86400fabf1a721a4bd5ad0da7fd2
zhoukang <zhoukang199191@126.com>
2016-03-01 23:57:51 +0800

    增加测试handler service

---
+++ src/org/cane/rpc/test/handlers/HelloServiceImpl.java
@@ -0,0 +1,16 @@
+/**
+ * @(#)HelloServiceImpl.java, 2016年3月1日. 
+ * 
+ * Copyright 2016 Yodao, Inc. All rights reserved.
+ * YODAO PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package org.cane.rpc.test.handlers;
+
+/**
+ *
+ * @author zhoukang
+ *
+ */
+public class HelloServiceImpl implements IHelloService {
+
+}
