166a724570f8138df2ae9bd747655b8f708e39c0
zhoukang <zhoukang199191@126.com>
2016-03-01 23:57:30 +0800

    增加bio server的逻辑；同时增加test 类

--- src/org/cane/rpc/server/BioRpcServer.java
+++ src/org/cane/rpc/server/BioRpcServer.java
@@ -6,16 +6,33 @@
  */
 package org.cane.rpc.server;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketAddress;
+
 /**
  *
  * @author zhoukang
  *
  */
 public class BioRpcServer extends RpcServer{
+    private int port;
+    private String ip;
 
     @Override
     public void start() {
-        
+        ServerSocket serverSocket = null; 
+        try {
+            serverSocket = new ServerSocket(port);
+            serverSocket.bind(new InetSocketAddress(ip , port));
+            while(true) {
+                Socket socket = serverSocket.accept();
+            }
+        } catch(IOException e) {
+            //TODO
+        }
     }
     
     @Override
---
+++ src/org/cane/rpc/test/TestBioRpcServer.java
@@ -0,0 +1,24 @@
+/**
+ * @(#)TestBioRpcServer.java, 2016年3月1日. 
+ * 
+ * Copyright 2016 Yodao, Inc. All rights reserved.
+ * YODAO PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package org.cane.rpc.test;
+
+/**
+ *
+ * @author zhoukang
+ *
+ */
+public class TestBioRpcServer {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
