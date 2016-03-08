04c2f91326b4355571041d98e7d41ee29932d743
zhoukang <zhoukang199191@126.com>
2016-03-08 22:36:44 +0800

    增加netty rpc server的单元测试

---
+++ src/org/cane/rpc/test/TestNettyRpcServer.java
@@ -0,0 +1,16 @@
+/**
+ * @(#)TestNettyRpcServer.java, 2016年3月8日. 
+ * 
+ * Copyright 2016 Yodao, Inc. All rights reserved.
+ * YODAO PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package org.cane.rpc.test;
+
+/**
+ *
+ * @author zhoukang
+ *
+ */
+public class TestNettyRpcServer {
+
+}
